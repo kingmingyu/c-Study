3c27072631620406f20701f7523702da2d4f5f83
202110910@sangmyung.kr <202110910@sangmyung.kr>
2023-11-27 15:40:26 +0900

    Study(23.11.27)

---
+++ 43Dictionary/Program.cs
@@ -0,0 +1,75 @@
+﻿class DicTest
+{
+    Dictionary<string, int> NewDicTest = new Dictionary<string, int>();
+
+    public void Print(string Name)
+    {
+        if (false == NewDicTest.ContainsKey(Name)) { //contains를 많이 이용해야 한다.
+            return;
+        }
+        Console.WriteLine(NewDicTest[Name]);
+    }
+    public void Add(string name, int value)
+    {
+        if (NewDicTest.ContainsKey(name)) {
+            return;
+        }
+        NewDicTest.Add(name, value);
+    }
+}
+
+namespace _43Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //연관 복합(배열형)
+            //string 키
+            //int 벨류(데이터)
+            //즉 키와 벨류를 연결해놓고 탐색에 편리한 자료구조이다.
+            Dictionary<string, int> NewDic = new Dictionary<string, int>();
+
+            NewDic.Add("일이에요", 1); //값의쌍이 연결
+            NewDic.Add("이에요", 2);
+            NewDic.Add("뭘까요 ?", 342347);
+            NewDic.Add("와와 재밌다.", 123);
+            //NewDic.Add("와와 재밌다.", 456); -> 똑같은 키를 2개 넣어줄 수 없다.(프로그램이 터진다.)
+            //딕셔너리는 같은 키를  넣어줄 일이 있다면 사용하지 마라.
+
+            //그에 따른 방어코드
+            if (false == NewDic.ContainsKey("일이에요")) {
+                NewDic.Add("일이에요", 1);
+            }
+
+            //인덱서 or 연산자 겹지정이라고 합니다.
+            Console.WriteLine(NewDic["일이에요"]); //딕셔너리는 이렇게 사용할 수 있다.(찾는 작업에 특화된 녀석이다.)
+            Console.WriteLine(NewDic["이에요"]);
+            //Console.WriteLine(NewDic["ㄴㅇ런ㅁ얄"]); <- 터진다.(key값이 없기 때문에.)
+
+            string Key = "당연히 없는값 테스트용";
+
+            if (NewDic.ContainsKey(Key)) { //Key라는 값이 있으면 NewDic에서 Key에 해당하는 값을 출력해라.(보통 이렇게 쓴다.)
+                Console.WriteLine(NewDic[Key]);
+            }
+
+            if (NewDic.ContainsKey("와와 재밌다.")) { //Key라는 값이 있으면 NewDic에서 Key에 해당하는 값을 출력해라.(보통 이렇게 쓴다.)
+                Console.WriteLine(NewDic["와와 재밌다."]);
+            }
+
+            NewDic.Remove("와와 재밌다."); //딕셔너리는 대부분 키를 갖고 조작.
+
+            //딕셔너리의 사용
+            //이처럼 딕셔너리는 클래스를 따로 만들어서 감싸는게 편하다.
+            DicTest NewTest = new DicTest();
+
+            NewTest.Add("하하하하", 1);
+            NewTest.Add("하하하하", 1);
+            NewTest.Add("하하하1", 1);
+            NewTest.Add("키입니다", 9999);
+
+            NewTest.Print("키입니다");
+            NewTest.Print("하하하1");
+        }
+    }
+}
---
+++ 44DataStructEx/Program.cs
@@ -0,0 +1,79 @@
+﻿class GTest<T>
+{
+    T Data;
+}
+
+namespace _44DataStructEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GTest < GTest < GTest<float> > New; <- 이런식으로 제네릭 안에 제네릭을 넣어줄 수 있다.
+            Dictionary<string, List<int>> NewDic;
+
+            List<Dictionary<string, LinkedList<Dictionary<string, string>>>> NewTest;
+
+            //이런경우에는 Dictionary쓰지 마라
+            //0, 1, 2, 3, 4, 5 -> Key
+            //Dictionary<int, int> --> 이럴거면 그냥 List를 써라.
+
+            //왠만하면 그냥 순회방식을 사용하라.
+            Dictionary<string, int> DicTest = new Dictionary<string, int>();
+            List<int> ListTest = new List<int>();
+            LinkedList<int> LinkedListTest = new LinkedList<int>();
+
+            DicTest.Add("일", 1);
+            DicTest.Add("이", 2);
+            DicTest.Add("삼", 3);
+            DicTest.Add("사", 4);
+            DicTest.Add("오", 5);
+
+            ListTest.Add(1);
+            ListTest.Add(2);
+            ListTest.Add(3);
+            ListTest.Add(4);
+            ListTest.Add(5);
+
+            LinkedListTest.AddLast(1);
+            LinkedListTest.AddLast(2);
+            LinkedListTest.AddLast(3);
+            LinkedListTest.AddLast(4);
+            LinkedListTest.AddLast(5);
+
+            //var은 그 시점에서 가장 알맞은 자료형이 되어 준다.(무엇이든지 될 수 있다.)
+            int Number = 100;
+            var AA = Number;
+
+
+            //foreach는 자동적으로 반복하는 문장으로서
+            //in을 통해서 각 배열이든 딕셔너리든 리스트든
+            //내부의 자료를 그대로 순회할 수 있게 처리해 준다.
+            //var은 그때의 가장 적합자료형으로 처리된다.
+            //in은 자료구조에서 빼온다라고만 이해해도 된다.
+            //그 대신 루프에 대한 제한을 하기가 어렵다.
+
+            //Tip) List와 LinkedList는 for문을, Dictionary는 foreach문을 이용하는 것이 좋다.(Dictionary는 for문 사용을 자제하는 것이 좋다.)
+            foreach (var item in ListTest) {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in LinkedListTest) {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in DicTest) {
+                Console.WriteLine(item);
+            }
+
+            foreach (KeyValuePair<string, int> item in DicTest) {
+                Console.WriteLine(item.Value);
+            }
+
+            foreach (KeyValuePair<string, int> item in DicTest) {
+                Console.WriteLine(item.Key);
+            }
+
+        }
+    }
+}
