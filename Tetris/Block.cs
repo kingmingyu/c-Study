f8e5240ec56b70498c1fc0f22c8db8c3d296491d
202110910@sangmyung.kr <202110910@sangmyung.kr>
2023-12-02 01:00:20 +0900

    Study(23.12.02)

---
+++ Tetris/Block.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//객체지향 어렵지 않다.
+//난 위에서부터 블럭이 떨어지는 것을 만들고 싶다.
+
+enum BLOCKTYPE
+{
+    BT_I, //짝대기
+    BT_L, //오른쪽 갈고리
+    BT_J, //왼쪽 갈고리
+    BT_Z, //왼쪽 삐뚫이
+    BT_S, //오른쪽 삐뚫이
+    BT_T, //법규
+    BT_O, //네모
+}
+class Block
+{
+    int X = 0;
+    int Y = 0;
+
+    List<List<string>> BlockData = new List<List<string>>();
+
+
+    TScreen Screen = null;
+
+    public Block(TScreen screen)
+    {
+        Screen = screen;
+
+        for (int y = 0; y < 4; y++) {
+            BlockData.Add(new List<string>());
+        }
+
+
+    }
+
+    private void Input()
+    {
+        //키를 눌렀다. 그러므로 Console.ReadKey()가 리턴된다.(맞긴 하지만 그전에도 이미 컴퓨터는 키를 체크하고 있다.)
+        if (Console.KeyAvailable == false) {
+            return;
+        }
+        switch (Console.ReadKey().Key) {
+            case ConsoleKey.A:
+                X -= 1;
+                break;
+            case ConsoleKey.D:
+                X += 1;
+                break;
+            case ConsoleKey.S:
+                Y += 1;
+                break;
+            default:
+                break;
+        }
+    }
+    public void Move()
+    {
+        //내가 어떤 키든 눌렀을 때만 input을 실행해라
+        Input();
+        for (int i = 0; i < 4; i++) {
+            Screen.SetBlock(Y+i, X, "■");
+        }
+    }
+}
+
+//상속을 내리고 가상함수를 쓴다.
+class StickBlock
+{
+
+}
+
+//상속을 내린다. (방법 1)
---
+++ Tetris/TScreen.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum BK
+{
+    W,
+    V,
+    B
+}
+
+class TScreen
+{
+    List<List<string>> BlockList = new List<List<string>>();
+
+    public void SetBlock(int y, int x, string BlockType)
+    {
+        BlockList[y][x] = BlockType;
+    }
+    public void Render()
+    {
+        for (int y = 0; y < BlockList.Count; y++) {
+            for (int x = 0; x < BlockList[y].Count; x++) {
+                Console.Write(BlockList[y][x]);
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    public void Clear()
+    {
+        for (int y = 0; y < BlockList.Count; y++) {
+            for (int x = 0; x < BlockList[y].Count; x++) {
+                if (y == 0 || y == BlockList.Count - 1) {
+                    BlockList[y][x] = "▣";
+                    continue;
+                }
+                BlockList[y][x] = "□";
+            }
+        }
+    }
+
+    public TScreen(int X, int Y)
+    {
+        for (int y = 0; y < Y; y++) {
+            BlockList.Add(new List<string>());
+            for (int x = 0; x < X; x++) {
+                BlockList[y].Add("□");
+            }
+        }
+        for (int i = 0; i < BlockList[BlockList.Count - 1].Count; i++) {
+            BlockList[0][i] = "▣";
+            BlockList[BlockList.Count - 1][i] = "▣";
+        }
+    }
+}
